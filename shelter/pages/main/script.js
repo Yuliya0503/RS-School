152aff32d9f3cf255dc93c0c691d06927822bd2f
--global <--global>
2022-04-21 11:52:40 +0300

    feat: add burger-menu for main page

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -4,12 +4,16 @@ const menuAdaptive = document.querySelector('.menu-adaptive');
 const menuBurger = document.querySelector('.menu-burger');
 const burgerMenu = document.querySelector('.burger-menu');
 const body = document.querySelector('.body');
+const div = document.querySelector('.div');
+
 function toggleMenu() {
   menuBurger.classList.toggle('open');
   menuAdaptive.classList.toggle('change');
   burgerMenu.classList.toggle('open-burger');
   body.classList.toggle('body-menu');
+  div.classList.toggle('shadow');
   
 }
 menuAdaptive.addEventListener('click', toggleMenu);
+
 
