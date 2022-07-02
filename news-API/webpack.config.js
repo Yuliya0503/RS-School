10c7dc45321f854886cf1241af745de0652f4a76
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-02 15:25:18 +0300

    feat: addtsconfig.json, add typescript, ESLint in webpack

--- news-API/webpack.config.js
+++ news-API/webpack.config.js
@@ -2,20 +2,42 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+//const ESLintPlugin = require('eslint-webpack-plugin');
+
+
+//const esLintPlugin = (isDev) => isDev ? [] : [ new ESLintPlugin({ extensions: ['ts', 'js'] }) ];
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
     mode: 'development',
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+              test: /\.s[ac]ss$/i,
+              use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+              test: /\.[tj]s$/,
+              use: 'ts-loader',
+              exclude: /node_modules/,
+            },
+            {
+              test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+              type: 'asset/resource',
+            },
+            {
+              test: /\.(woff(2)?|eot|ttf|otf)$/i,
+              type: 'asset/resource',
             },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
@@ -26,7 +48,8 @@ const baseConfig = {
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
         }),
-        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: '[name].css' }),
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     ],
 };
 
