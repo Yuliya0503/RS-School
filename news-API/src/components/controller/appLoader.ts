7120400765803156d916c066f69fed08161110af
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-03 16:59:26 +0300

    feat: add apiKey in appLoader.ts

--- news-API/src/components/controller/appLoader.ts
+++ news-API/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '70c2ecf9bc9546b18e5ba711d91e6ec5', // получите свой ключ https://newsapi.org/
         });
     }
 }
