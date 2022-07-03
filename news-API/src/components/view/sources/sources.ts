46c73b3012f22c21a6d17fa5e12c9161417c1179
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-03 19:28:51 +0300

    feat: add typescript to sources.ts file

--- news-API/src/components/view/sources/sources.ts
+++ news-API/src/components/view/sources/sources.ts
@@ -1,20 +1,30 @@
 import './sources.css';
 
+interface Data {
+  id: string;
+  name: string;
+}
 class Sources {
-    draw(data) {
-        const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+    draw(data: Data[]):void {
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        interface Items {
+          id: string;
+          name: string;
+      };
 
         data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true);
 
-            sourceClone.querySelector('.source__item-name').textContent = item.name;
-            sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
+
+            sourceClone.querySelector('.source__item-name')!.textContent = item.name;
+            sourceClone.querySelector('.source__item')!.setAttribute('data-source-id', item.id);
 
             fragment.append(sourceClone);
         });
 
-        document.querySelector('.sources').append(fragment);
+        document.querySelector('.sources')!.append(fragment);
     }
 }
 
