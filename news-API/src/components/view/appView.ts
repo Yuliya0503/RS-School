876bec70002b41c536ccb7839f40bb42f7582e29
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-04 13:49:43 +0300

    feat: add typescript in appView.ts

--- news-API/src/components/view/appView.ts
+++ news-API/src/components/view/appView.ts
@@ -1,18 +1,42 @@
 import News from './news/news';
 import Sources from './sources/sources';
 
+interface Data {
+  _item: string;
+  idx: number;
+  urlToImage: string;
+  author: string;
+  publishedAt: string;
+  title: string;
+  description: string;
+  url: string;
+  source: { name: string};
+}
+
+interface Source {
+  id: string;
+  name: string;
+}
+interface DataNews {
+  status: string;
+  totalResults: number;
+  articles: Array<Data>;
+  sources: Array<Source>;
+}
 export class AppView {
+  news: News;
+  sources: Sources;
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: DataNews): void {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: DataNews): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
