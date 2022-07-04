94850cc4fef8edc330de1246694cd622d3c97826
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-04 21:52:18 +0300

    feat: change url

--- news-API/src/components/controller/appLoader.ts
+++ news-API/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '70c2ecf9bc9546b18e5ba711d91e6ec5', // получите свой ключ https://newsapi.org/
         });
     }
