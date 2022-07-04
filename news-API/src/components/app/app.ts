6eae7487ed56dea815d9a3f1ee41d22194467841
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-04 15:53:21 +0300

    feat: add typescript in app file

--- news-API/src/components/app/app.ts
+++ news-API/src/components/app/app.ts
@@ -1,17 +1,21 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import {DataNews } from '../view/appView';
+
 
 class App {
+  controller: AppController;
+  view: AppView;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+    start(): void {
+        (document
+            .querySelector('.sources') as HTMLElement)
+            .addEventListener('click', (e: Event) => this.controller.getNews(e, (data: DataNews) => this.view.drawNews(data)));
+        this.controller.getSources((data: DataNews) => this.view.drawSources(data));
     }
 }
 
