f4bed476996a6d3a9daef26fb018d14f984e536f
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-05 20:24:39 +0300

    feat: add private in property and add types in app.ts

--- news-API/src/components/app/app.ts
+++ news-API/src/components/app/app.ts
@@ -1,11 +1,11 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
-import {DataNews } from '../view/appView';
+import { DataNews } from '../view/appView';
 
 
 class App {
-  controller: AppController;
-  view: AppView;
+   private controller: AppController;
+   private view: AppView;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
@@ -14,8 +14,8 @@ class App {
     public start(): void {
         (document
             .querySelector('.sources') as HTMLElement)
-            .addEventListener('click', (e: Event) => this.controller.getNews(e, (data: DataNews) => this.view.drawNews(data)));
-        this.controller.getSources((data: DataNews) => this.view.drawSources(data));
+            .addEventListener<'click'>('click', (e: Event): void => this.controller.getNews<DataNews>(e, (data: DataNews) => this.view.drawNews(data)));
+        this.controller.getSources<DataNews>((data: DataNews): void => this.view.drawSources(data));
     }
 }
 
