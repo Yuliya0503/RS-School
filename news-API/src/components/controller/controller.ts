77398b3ad7e9b74d8d48676f47ea642d9a0b5c94
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-05 20:27:51 +0300

    feat: add access modifiers and types in controller.ts

--- news-API/src/components/controller/controller.ts
+++ news-API/src/components/controller/controller.ts
@@ -10,13 +10,13 @@ class AppController extends AppLoader {
         );
     }
 
-    public getNews<T>(e: Event, callback:  (data: T) => void) {
+    public getNews<T>(e: Event, callback: (data: T) => void) {
         let target = e.target as HTMLElement;
         const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId: string = target.getAttribute('data-source-id');
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId as string);
                     super.getResp(
