d777aacae9ee6c7a6c3503da8dd03fccf8163dc1
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-09 10:25:29 +0300

    fix: delete the main.css file from the dist folder, change the location of the dist folder

--- news-API/webpack.config.js
+++ news-API/webpack.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index.ts'),
@@ -10,8 +11,8 @@ const baseConfig = {
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+              test: /\.css$/,
+              use: ['style-loader', 'css-loader'],
             },
             {
               test: /\.s[ac]ss$/i,
@@ -37,15 +38,21 @@ const baseConfig = {
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, './dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
         }),
-        new MiniCssExtractPlugin({ filename: '[name].css' }),
+        new MiniCssExtractPlugin(),
+
         new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new CopyPlugin({
+          patterns: [
+            { from: './src/assets', to: './' },
+          ],
+        }),
     ],
 };
 
