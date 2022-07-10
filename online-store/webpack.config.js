53bd6a72b973d9f122c5b6dd96027270e99faac4
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-10 15:04:58 +0300

    init: start online-store task

---
+++ online-store/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  mode: 'development',
+  module: {
+      rules: [
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+          },
+          {
+            test: /\.[tj]s$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+          },
+          {
+            test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.(woff(2)?|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },
+      ],
+  },
+  resolve: {
+      extensions: ['.ts', '.js'],
+  },
+  output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, './src/index.html'),
+          filename: 'index.html',
+      }),
+      new MiniCssExtractPlugin(),
+
+      new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+      new CopyPlugin({
+        patterns: [
+          { from: './src/assets', to: './' },
+        ],
+      }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
