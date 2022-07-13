f23f96b1b5ae59911cbfafdabc967dc6725ce929
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-13 19:58:28 +0300

    feat: add style products

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,4 +1,4 @@
-import './global.css';
+import './global.scss';
 import Products from './scripts/components/products';
 
 const productsPage = new Products();
--- online-store/src/scripts/components/products.ts
+++ online-store/src/scripts/components/products.ts
@@ -18,25 +18,25 @@ export default class Products implements CatalogInterface {
         CATALOG.forEach(({ id, name, img, quantity, year, cover, genre, publishingHouse, favorite }) => {
 
           htmlCatalog += `
-            <div>
-              <h4>${name}</h4>
-              <div>
+            <div class="products-element">
+              <h4 class="products-element__name">${name}</h4>
+              <div class="products-element__img">
                 <img src="${img}" alt="${name}">
               </div>
-              <ul>
-                <li>Количество: ${quantity}</li>
-                <li>Год издания: ${year}</li>
-                <li>Переплет: ${cover}</li>
-                <li>Жанр: ${genre}</li>
-                <li>Издательство: ${publishingHouse}</li>
-                <li>Популярность: ${favorite} </li>
+              <ul class="products-element__description">
+                <li class="products-element__description_quantity">Количество: ${quantity}</li>
+                <li class="products-element__description_year">Год издания: ${year}</li>
+                <li class="products-element__description_cover">Переплет: ${cover}</li>
+                <li class="products-element__description_genre">Жанр: ${genre}</li>
+                <li class="products-element__description_publishingHouse">Издательство: ${publishingHouse}</li>
+                <li class="products-element__description_favorite">Популярность: ${favorite} </li>
               </ul>
-              <button class="button" data-id="${id}">Добавить в корзину</button>
+              <button class="products-element__button button" data-id="${id}">Добавить в корзину</button>
             </div>`;
         });
 
         const html: string = `
-    <div>
+    <div class="products-container">
       ${htmlCatalog}
     </div>`;
 
