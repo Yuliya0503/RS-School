a6aac2850f462678e452f4685b90316a2d4a763f
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-18 13:22:21 +0300

    feat: add input text

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -3,6 +3,9 @@ import './global.scss';
 import LocalStorage from './scripts/localStorage/localStorage';
 import Products from './scripts/components/products';
 import Sorting from './scripts/components/sorting';
+import Filter from './scripts/components/filters';
+import Input from './scripts/components/input';
+
 
 const localStorage = new LocalStorage();
 localStorage.getProd();
@@ -13,3 +16,9 @@ productsPage.render();
 
 const sort = new Sorting();
 sort.init();
+
+const filter = new Filter();
+filter.init();
+
+const input = new Input();
+input.filter();
---
+++ online-store/src/scripts/components/input.ts
@@ -0,0 +1,17 @@
+export default class Input {
+  filter() {
+    let input = document.getElementById('filter-input');
+    input.addEventListener('keyup', function(){
+      let filter = (input as HTMLInputElement).value.toLowerCase(),
+      filterElements = document.querySelectorAll('.products-element__name');
+      filterElements.forEach(item => {
+        if(item.innerHTML.toLowerCase().indexOf(filter) > -1) {
+          (item.parentNode as HTMLElement).style.display = '';
+        } else {
+          (item.parentNode as HTMLElement).style.display = 'none';
+        }
+
+      })
+    })
+  }
+}
