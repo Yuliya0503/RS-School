e36665e459b8afcc8403e8d0806daf04fb1ac81c
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-18 14:14:40 +0300

    refactor: apply eslint

--- online-store/src/scripts/components/input.ts
+++ online-store/src/scripts/components/input.ts
@@ -1,17 +1,16 @@
 export default class Input {
-  filter() {
-    let input = document.getElementById('filter-input');
-    input.addEventListener('keyup', function(){
-      let filter = (input as HTMLInputElement).value.toLowerCase(),
-      filterElements = document.querySelectorAll('.products-element__name');
-      filterElements.forEach(item => {
-        if(item.innerHTML.toLowerCase().indexOf(filter) > -1) {
-          (item.parentNode as HTMLElement).style.display = '';
-        } else {
-          (item.parentNode as HTMLElement).style.display = 'none';
-        }
-
-      })
-    })
-  }
+    filter() {
+        const input = document.getElementById('filter-input');
+        input.addEventListener('keyup', function () {
+            const filter = (input as HTMLInputElement).value.toLowerCase(),
+                filterElements = document.querySelectorAll('.products-element__name');
+            filterElements.forEach((item) => {
+                if (item.innerHTML.toLowerCase().indexOf(filter) > -1) {
+                    (item.parentNode as HTMLElement).style.display = '';
+                } else {
+                    (item.parentNode as HTMLElement).style.display = 'none';
+                }
+            });
+        });
+    }
 }
