eff845fd54fe7a49af44d56a2f7e24f133b0b18c
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-07-18 17:46:25 +0300

    refactor: apply eslint

--- online-store/src/scripts/components/filters.ts
+++ online-store/src/scripts/components/filters.ts
@@ -1,31 +1,25 @@
-//import { CATALOG } from "../catalog/catalog";
-import { CATALOG } from "../catalog/catalog";
-import { Card } from "./card";
-
 export default class Filter {
-  buttons: NodeListOf<HTMLElement> = document.querySelectorAll('.filter-button');
-  cards: NodeListOf<HTMLElement> = document.querySelectorAll('.products-element');
+    buttons: NodeListOf<HTMLElement> = document.querySelectorAll('.filter-button');
+    cards: NodeListOf<HTMLElement> = document.querySelectorAll('.products-element');
 
-  filter(category: string, items: NodeListOf<HTMLElement>) {
-    items.forEach((item)=> {
-      //const isShowAll = category.toLowerCase() === 'reset';
-      const isItemFiltered = !item.classList.contains(category);
-      if(isItemFiltered /* && !isSowAll*/) {
-        item.classList.add('hide');
-      } else {
-        item.classList.remove('hide');
-      }
-    })
-  }
+    filter(category: string, items: NodeListOf<HTMLElement>) {
+        items.forEach((item) => {
+            //const isShowAll = category.toLowerCase() === 'reset';
+            const isItemFiltered = !item.classList.contains(category);
+            if (isItemFiltered /* && !isSowAll*/) {
+                item.classList.add('hide');
+            } else {
+                item.classList.remove('hide');
+            }
+        });
+    }
 
-  app() {
-    this.buttons.forEach((button)=>{
-      button.addEventListener('click', ()=> {
-        const currentCategory = button.dataset.id;
-        this.filter(currentCategory, this.cards);
-      })
-    })
-  }
-
+    app() {
+        this.buttons.forEach((button) => {
+            button.addEventListener('click', () => {
+                const currentCategory = button.dataset.id;
+                this.filter(currentCategory, this.cards);
+            });
+        });
+    }
 }
-
