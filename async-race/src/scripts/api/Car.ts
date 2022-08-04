2db9e0dea56b0dd722cce4bd612584e24b577f9f
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-08-04 12:58:08 +0300

    feat: add class Car

---
+++ async-race/src/scripts/api/Car.ts
@@ -0,0 +1,40 @@
+import { garage } from './ApiLinks';
+import { ICar } from '../models/interfases';
+
+export default class Car {
+    async getCar<T>(id: ICar['id']): Promise<T> {
+        const carResponse: Response = await fetch((`${garage}/${id}`), {
+          method: 'GET',
+        });
+        return carResponse.json();
+    }
+
+    async createCar<T>(carInformation: ICar['carInformation']): Promise<T> {
+        const carCreate: Response = await fetch(garage, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: 'POST',
+            body: JSON.stringify(carInformation),
+        });
+        return carCreate.json();
+    }
+
+    async changeCar<T>(id: ICar['id'], carInformation: ICar['carInformation']): Promise<T> {
+        const carChange: Response = await fetch(`${garage}/${id}`, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: 'PUT',
+            body: JSON.stringify(carInformation),
+        });
+        return carChange.json();
+    }
+
+    async deleteCar<T>(id: ICar['id']): Promise<T> {
+        const carDelete: Response = await fetch(`${garage}/${id}`, {
+            method: 'DELETE',
+        });
+        return carDelete.json();
+    }
+}
